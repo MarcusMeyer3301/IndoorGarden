8057aae892709ed6675cfc4c5d927349c851c78d
MarcusMeyer3301 <marcus.meyer@ucalgary.ca>
2025-01-28 19:38:29 -0700

    Basic webserver
    Created the basics for manually controlling the pump through a website hosted on the pi pico.

--- picoWebServer.py
+++ picoWebServer.py
@@ -6,15 +6,116 @@ from machine import Pin
 
 
 pump = Pin(16, Pin.OUT)
+pump.value(0)
+
 pump_state = False
+
+garden_state = True
+garden_state_readable = "Automated"
+
+light_state = True
+
 
 ssid = "Woodside"
 password = "172-Wood"
 
-def webpage(pump_state):
-    html = f'''<h1>Grass Grower &#128511;</h1>
-        <h3>Pump<h3>
-        <h4>{pump_state}<h4>
+def webpage(pump_state, garden_state, garden_state_readable):
+    if not garden_state:
+        manual_mode_html = f'''<br>
+    <br>
+    <div class="operation-mode" style="justify-content: space-around;">
+        <div class="box1">
+            <h3>Pump State: {pump_state}</h3>
+            <a href="pumpon"><button class="btn">On</button></a>
+            <a href="pumpoff"><button class="btn">Off</button></a> 
+        </div>
+        <div class="box2">
+            <h3>Grow Light State: {light_state}</h3>
+            <a href="lighton"><button class="btn">On</button></a>
+            <a href="lightoff"><button class="btn">Off</button></a> 
+        </div>
+    </div>
+        '''
+    else:
+        manual_mode_html = ""
+
+    html = f'''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Indoor Garden</title>
+</head>
+<body>
+    <br>
+    <h1 id="title">Welcome to your Indoor Garden!</h1>
+    <br>
+    <div class="operation-mode">
+        <h3>Current Operation mode: {garden_state_readable}</h3>
+        <a href="automated"><button class="btn">Automated</button></a>
+        <a href="manual"><button class="btn">Manual</button></a>
+        
+    </div>
+
+    {manual_mode_html}
+
+</body>
+
+<style>
+    :root{{
+        --brown: #8D6E63;
+        --light-green: #52d656;
+        --dark-green: #219c27;
+    }}
+
+    .box1{{
+        display: inline-block;
+        border-color: #388E3C;
+        border-width: 2px;
+        border-style: double;
+        padding: 10px;
+        margin-right: 20px;
+    }}
+
+    .box2{{
+        display: inline-block;
+        border-color: #388E3C;
+        border-width: 2px;
+        border-style: double;
+        padding: 10px;
+        margin-left: 20px;
+
+    }}
+
+    body{{
+        background-color: var(--brown);
+    }}
+
+    .operation-mode{{
+        background-color: var(--light-green);
+        margin-left: 20%;
+        margin-right: 20%;
+        text-align: center;
+        padding: 20px;
+        border-color: #9aff9d;
+        border-width: 2px;
+        border-style: dashed;
+    }}
+    
+    #title{{
+        text-align: center; 
+        color: whitesmoke;
+        text-shadow: 5px 5px 3px #000000;
+    }}
+
+    .btn{{
+        border-radius: 50%;
+        padding: 10px;
+        border-color: #388E3C;
+        background-color: var(--dark-green)
+    }}
+</style>
+</html>
     '''
 
     return str(html)
@@ -76,18 +177,24 @@ while True:
 
         elif request == "/lighton":
             print("light on")
+            light_state = True
 
         elif request == "/lightoff":
             print("light off")
+            light_state = False
 
         elif request == "/automated":
             print("running in automated mode")
+            garden_state = True
+            garden_state_readable = "Automated"
 
         elif request == "/manual":
             print("Running in manual mode")
+            garden_state = False
+            garden_state_readable = "Manual"
 
 
-        resp = webpage(pump_state)
+        resp = webpage(pump_state, garden_state, garden_state_readable)
         conn.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
         conn.send(resp)
         conn.close()
