0c355ed46d943d9deca1536055d9b5d36a09a6eb
MarcusMeyer3301 <marcus.meyer@ucalgary.ca>
2025-02-07 15:33:37 -0700

    Added moisture button

--- picoWebServer.py
+++ picoWebServer.py
@@ -2,8 +2,19 @@ import network
 import socket
 import time
 import random
-from machine import Pin
+from machine import Pin, ADC, I2C
+from stemma_soil_sensor import StemmaSoilSensor
 
+SDA_PIN = 4 # GPIO 4 
+SCL_PIN = 5 # GPIO 5
+
+
+i2c = I2C(0, sda=Pin(SDA_PIN), scl=Pin(SCL_PIN), freq=400000)
+
+time.sleep(2)
+stemma_soil_sensor = StemmaSoilSensor(i2c)
+seesaw = stemma_soil_sensor # Data Sheet = ~200 (very dry) to ~2000 (very wet) 1015 = Wet, 331 = Dry
+time.sleep(2)
 
 pump = Pin(16, Pin.OUT)
 pump.value(0)
@@ -13,6 +24,7 @@ light = Pin(15, Pin.OUT)
 light.value(0)
 light_state = False
 
+photo_res = ADC(Pin(27))
 garden_state = True
 garden_state_readable = "Automated"
 
@@ -21,7 +33,7 @@ garden_state_readable = "Automated"
 ssid = "Droid"
 password = "password123"
 
-def webpage(pump_state, garden_state, garden_state_readable):
+def webpage(pump_state, garden_state, garden_state_readable, moisture):
     if not garden_state:
         manual_mode_html = f'''<br>
     <br>
@@ -56,8 +68,9 @@ def webpage(pump_state, garden_state, garden_state_readable):
         <h3>Current Operation mode: {garden_state_readable}</h3>
         <a href="automated"><button class="btn">Automated</button></a>
         <a href="manual"><button class="btn">Manual</button></a>
-        
+        <h2 id="moisture-value">Moisture {moisture}</h2>
     </div>
+    <button onclick="loadMoistureValue()"> LOAD MOISTURE </button>
 
     {manual_mode_html}
 
@@ -117,6 +130,18 @@ def webpage(pump_state, garden_state, garden_state_readable):
         background-color: var(--dark-green)
     }}
 </style>
+<script>
+function loadMoistureValue(){{
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {{
+        document.getElementById("moisture-value").innerHTML = this.responseText;
+        
+    }}
+    xhttp.open("GET", "/moisture", true);
+    xhttp.send();
+}}
+
+</script>
 </html>
     '''
 
@@ -197,11 +222,22 @@ while True:
             garden_state = False
             garden_state_readable = "Manual"
 
+        elif request == "/moisture":
+            moisture = seesaw.get_moisture()
+            temperature = seesaw.get_temp()
+            resp = (str(moisture))
+            conn.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+            conn.send(resp)
+            conn.close()
+            
 
-        resp = webpage(pump_state, garden_state, garden_state_readable)
+
+        resp = webpage(pump_state, garden_state, garden_state_readable, 0)
         conn.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
         conn.send(resp)
         conn.close()
+        
+
 
     except OSError as e:
         conn.close()
